78d4c6d2254f73761a3bf999788eee836b69a8cf
myapos <myapos@gmail.com>
2022-04-26 14:51:22 +0300

    generated 24 combinations

--- utils/checkArrayExistence.js
+++ utils/checkArrayExistence.js
@@ -1,14 +1,31 @@
 import * as utils from "./";
 
+//! it will accept a list of arrays and check if an array exists in the list
+
 const checkArrayExistence = ({ list, array }) => {
+  let arrayExists = false;
   const hasLength = list.length > 0;
-  return (
-    hasLength &&
-    list.reduce((acc, val) => {
-      console.log(utils.compareArrays({ arr1: array, arr2: val }));
-      return utils.compareArrays({ arr1: array, arr2: val }) && acc;
-    }, true)
-  );
+  // return (
+  //   hasLength &&
+  //   list.reduce((acc, val) => {
+  //     // console.log(utils.compareArrays({ arr1: array, arr2: val }));
+  //     return utils.compareArrays({ arr1: array, arr2: val }) && acc;
+  //   }, true)
+  // );
+
+  if (!hasLength) {
+    return false;
+  }
+
+  for (let i = 0; i < list.length; i++) {
+    arrayExists = utils.compareArrays({ arr1: array, arr2: list[i] });
+
+    if (arrayExists) {
+      break;
+    }
+  }
+
+  return arrayExists;
 };
 
 export default checkArrayExistence;
--- utils/generatePossibleRanges.js
+++ utils/generatePossibleRanges.js
@@ -22,27 +22,160 @@ const generatePossibleRanges = (range) => {
   }
 
   let arrayToPop = utils.deepCopy(firstArray);
-  for (let i = 0; i < firstArray.length; i++) {
-    arrayToPop = utils.popArray(arrayToPop);
-    console.log("arrayToPop", arrayToPop);
 
+  for (let i = 0; i < firstArray.length; i++) {
     const arrayIsFullPoped = utils.compareArrays({
       arr1: arrayToPop,
       arr2: firstArray,
     });
 
-    if (arrayIsFullPoped) {
+    if (arrayIsFullPoped && i > 0) {
       console.log("is fully poped", i);
+    }
+
+    combinations = [
+      ...utils.generateCombinationSwaps({
+        array: arrayToPop,
+        combinations,
+      }),
+    ];
+
+    arrayToPop = utils.popArray(arrayToPop);
+    console.log("arrayToPop", arrayToPop);
+  }
+
+  //!intermediary swaps A
+  for (let i = firstArray.length - 1; i >= 0; i--) {
+    let baseIndex = i;
+    let previousOfBaseIndex = i - 1;
+
+    if (previousOfBaseIndex >= 0) {
+      const swappedArray = utils.swapElementsInArray(
+        firstArray,
+        baseIndex,
+        previousOfBaseIndex
+      );
+
+      console.log("swappedArray", swappedArray);
+
+      combinations = [
+        ...utils.generateCombinationSwaps({
+          array: swappedArray,
+          combinations,
+        }),
+      ];
     }
   }
 
+  //! swaps from the end
+  for (let i = firstArray.length - 1; i >= 0; i--) {
+    let baseIndex = firstArray.length - 1;
+    let previousOfBaseIndex = i - 1;
+
+    if (previousOfBaseIndex >= 0) {
+      const swappedArray = utils.swapElementsInArray(
+        firstArray,
+        baseIndex,
+        previousOfBaseIndex
+      );
+
+      console.log("swappedArray", swappedArray);
+
+      combinations = [
+        ...utils.generateCombinationSwaps({
+          array: swappedArray,
+          combinations,
+        }),
+      ];
+    }
+  }
+
+  // xiasti
+
+  // diades
+
+  for (let i = 0; i <= firstArray.length - 1; i++) {
+    const length = firstArray.length - 1;
+    const pairA = [i, i + 1];
+    const pairB = [length - i, length - i - 1];
+
+    let baseIndex = firstArray.length - 1;
+    let previousOfBaseIndex = i - 1;
+
+    const isInRange =
+      pairA[0] <= length &&
+      pairA[1] <= length &&
+      pairB[0] >= 0 &&
+      pairB[1] >= 0;
+
+    if (isInRange) {
+      const swappedArray = utils.swapElementsInArray(
+        utils.swapElementsInArray(firstArray, pairA[0], pairA[1]),
+        pairB[0],
+        pairB[1]
+      );
+
+      console.log("swappedArray", swappedArray);
+
+      combinations = [
+        ...utils.generateCombinationSwaps({
+          array: swappedArray,
+          combinations,
+        }),
+      ];
+    }
+  }
+
+  const sumPairs = [];
+  for (let i = 0; i <= firstArray.length - 1; i++) {
+    for (let j = 0; j <= firstArray.length - 1; j++) {
+      if (i + j === firstArray.length - 1) {
+        console.log("found pair", firstArray.length, " i", i, " j ", j);
+        sumPairs.push([i, j]);
+      }
+    }
+  }
+
+  for (let i = 0; i <= sumPairs.length - 1; i++) {
+    let j = i + 1;
+
+    if (j < sumPairs.length - 1) {
+      let swappedArray = utils.swapElementsInArray(
+        utils.swapElementsInArray(firstArray, sumPairs[i][0], sumPairs[i][1]),
+        sumPairs[j][0],
+        sumPairs[j][1]
+      );
+
+      console.log("swappedArray", swappedArray);
+
+      combinations = [
+        ...utils.generateCombinationSwaps({
+          array: swappedArray,
+          combinations,
+        }),
+      ];
+    }
+  }
+  console.log("sumPairs", sumPairs);
+  /* 
+  
+  swappedArray = utils.swapElementsInArray(
+    utils.swapElementsInArray(firstArray, 0, 3),
+    1,
+    2
+  );
+
+  console.log("swappedArray", swappedArray);
+
   combinations = [
     ...utils.generateCombinationSwaps({
-      array: firstArray,
+      array: swappedArray,
       combinations,
     }),
-  ];
+  ]; */
+
   console.log("combinations", combinations);
+  console.log("num of combinations", combinations.length);
 };
 
 export default generatePossibleRanges;
