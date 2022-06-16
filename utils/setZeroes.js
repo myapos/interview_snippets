5028be791547a8e04ff4ad9632d5bdcb6da27a98
myapos <myapos@gmail.com>
2022-06-16 10:05:41 +0300

    comments in setZeroes

--- utils/setZeroes.js
+++ utils/setZeroes.js
@@ -7,14 +7,15 @@ const setZeroes = (matrix) => {
   for (let row = 0; row < matrix.length; row++) {
     for (let col = 0; col < matrix[0].length; col++) {
       if (matrix[row][col] === 0) {
-        // zeroes.push([row, col]);
         for (let i = 0; i < matrix[0].length; i++) {
+          //! do not touch the initial zeroes
           if (matrix[row][i] !== 0) {
             matrix[row][i] = flag;
           }
         }
 
         for (let j = 0; j < matrix.length; j++) {
+          //! do not touch the initial zeroes
           if (matrix[j][col] !== 0) {
             matrix[j][col] = flag;
           }
